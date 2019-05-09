221fc378b498d72eb7336007b88b1dc414d3351e
Florian.Windolf <florian.windolf@tum.de>
2019-05-09 12:57:33 +0200

    Show off more methods in example

--- examples/SimpleImage/main.cpp
+++ examples/SimpleImage/main.cpp
@@ -15,12 +15,28 @@ int main(int argc, char** argv)
 
     std::string color_file = std::string(SOURCE_DIR) + "/examples/data/image.png";
     Image<uchar3> color(color_file);
+    Image<float> grey = color.asGray<uchar>().as<float>();
 
     std::cout << "Color image (" << color.width() << "x" << color.height() << ")" << std::endl;
 
-    std::cout << "min = " << color.min() << std::endl 
-        << "max = " << color.max() << std::endl
-        << "mean =" << color.sum() / color.size() << std::endl;
+    std::cout << "num = " << color.size() << std::endl;
+    std::cout << "valid = " << color.valid() << std::endl;
+    std::cout << "nonzero = " << color.nonzero() << std::endl;
+   
+    std::cout << "min = " << color.min() << std::endl;
+    std::cout << "max = " << color.max() << std::endl;
+    std::cout << "mean = " << color.mean() << std::endl;
+    std::cout << "norm1 = " << color.norm1() << std::endl;
+    std::cout << "norm2 = " << color.norm2() << std::endl;
+
+
+    std::cout << "min = " << grey.min() << std::endl;
+    std::cout << "max = " << grey.max() << std::endl;
+    std::cout << "mean = " << grey.mean() << std::endl;
+    std::cout << "median= " << grey.median() << std::endl;
+    std::cout << "norm1 = " << grey.norm1() << std::endl;
+    std::cout << "norm2 = " << grey.norm2() << std::endl;
 
     color.show("Color", COLOR_TYPE_RGB);
+    grey.show("Grey", COLOR_TYPE_GREY_F);
 }
